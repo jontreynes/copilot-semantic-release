326bcb865d41e858c750a455ce21c4d349f263be
Jonathan Raynes <Jonathan.Raynes@ecolab.com>
2026-01-23 17:25:53 -0500

    fix: update version to 0.1.1 and add @semantic-release/changelog dependency

--- release.config.js
+++ release.config.js
@@ -3,8 +3,9 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     ["@semantic-release/git", {
-      "assets": ["dist/*.js", "dist/*.js.map"],
+      "assets": ["package.json", "package-lock.json", "CHANGELOG.md", "dist/*.js", "dist/*.js.map"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     '@semantic-release/github'
