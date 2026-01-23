f5fa5eb589adf3e2e8fc0d0834834ee67e8174cf
Jonathan Raynes <Jonathan.Raynes@ecolab.com>
2026-01-23 18:32:55 -0500

    feat!: test release

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const core = require('@actions/core');
 
 const person = core.getInput('person');
 
-console.log(`Hey, welcome ${person}! Here's a test part 4. `);
+console.log(`Hey, welcome ${person}! Here's a test part 5`);
