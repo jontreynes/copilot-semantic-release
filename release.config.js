f94a55a6a806fd3f6c6f59858e844ae58368e68c
Jonathan Raynes <Jonathan.Raynes@ecolab.com>
2026-01-23 18:40:12 -0500

    fix: simplify semantic-release config to use default Angular preset

--- release.config.js
+++ release.config.js
@@ -1,7 +1,9 @@
 const config = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ["@semantic-release/commit-analyzer", {
+      "preset": "angular"
+    }],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     ["@semantic-release/npm", {
