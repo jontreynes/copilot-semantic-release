a3468a163ceaacb934a43e71aac7e4ded52c2317
Jonathan Raynes <Jonathan.Raynes@ecolab.com>
2026-01-23 22:23:03 -0500

    fix: update urgent fix log message to include 'newfix'

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ const person = core.getInput('person');
 console.log(`Hey, welcome ${person}! Here's a test part 7 - urgent hotfix applied`);
 
 // Hotfix v3.6.7: Added urgent fix
-console.log('Urgent fix applied successfully!');
+console.log('Urgent fix applied successfully!newfix');
