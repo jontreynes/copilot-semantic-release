e8eebed25d9c2f71f6840b48e06c830e4f536fd8
Jonathan Raynes <Jonathan.Raynes@ecolab.com>
2026-01-23 21:21:03 -0500

    fix: update welcome message to reflect test part 7

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const core = require('@actions/core');
 
 const person = core.getInput('person');
 
-console.log(`Hey, welcome ${person}! Here's a test part 6`);
+console.log(`Hey, welcome ${person}! Here's a test part 7`);
