6d23f8bb386be7f1a2f81311f5046e29c1df9340
Jonathan Raynes <Jonathan.Raynes@ecolab.com>
2026-01-26 15:58:19 -0500

    feat: add npm release configuration to semantic-release setup

--- release.config.js
+++ release.config.js
@@ -77,6 +77,13 @@ const config = {
       }
     }],
     '@semantic-release/changelog',
+    ["@semantic-release/npm", {
+      "npmPublish": false  // Updates package.json but doesn't publish to npm
+    }],
+    ["@semantic-release/git", {
+      "assets": ["package.json", "package-lock.json", "CHANGELOG.md"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
     ["@semantic-release/github", {
       "assets": [
         {"path": "release-artifacts/*.tar.gz", "label": "Release Artifact"},
